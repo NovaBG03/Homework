acd3961c8306f00176a8e54a786895a7aafd3e08
Nikita Koynov <nikicha03@gmail.com>
2018-09-29 12:19:12 +0300

    27.09.2018 HomeWork
    Лице и обиколка на кръг и триъгълник

---
+++ 27.09.2018/27.09.2018/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Обиколка_и_лице_на_триъгълник
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedete ednata strana na triugulnik i visochinata kum neq");
+            Console.Write("a = ");
+            var a = double.Parse(Console.ReadLine());
+            Console.Write("ha = ");
+            var ha = double.Parse(Console.ReadLine());
+            Console.WriteLine("Vuvedete drugite dve strani");
+            Console.Write("b = ");
+            var b = double.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            var c = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.Write("P = "); Console.WriteLine(a + b + c);
+            Console.Write("S = "); Console.WriteLine((a*ha)/2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 27.09.2018/Обиколка и лице на кръг/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Обиколка_и_лице_на_кръг
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedete radiusa");
+            Console.Write("r = ");
+            var r = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.Write("P = ");
+            Console.WriteLine(2 * 3.14 * r);
+            Console.Write("S = ");
+            Console.WriteLine(3.14 * r * r);
+            Console.ReadLine();
+        }
+    }
+}
