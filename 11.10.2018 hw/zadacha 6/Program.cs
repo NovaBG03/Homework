dde458237466fbb468ae5b4d6768bf072f96ba7d
Nikita Koynov <nikicha03@gmail.com>
2018-10-13 08:17:09 +0300

    zadacha6
    uprajnenie na usloven operator if

---
+++ 11.10.2018 hw/zadacha 6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = a;
+            if (b <= 100)
+            {
+                a = a + 5;
+            }
+            else
+            {
+                a = a + a*120/100;
+                if(b > 1000)
+                {
+                    a = a * 10 / 100;
+                }
+            }
+            if (b%2 == 0)
+            {
+                a = a + 1;
+            }
+            if (b%10 == 5)
+            {
+                a = a + 2;
+            }
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
