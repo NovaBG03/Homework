d17723ab0ac39341312dc24d81667325a9dff682
Nikita Koynov <nikicha03@gmail.com>
2018-10-22 22:52:58 +0300

    new work
    yey switch test

---
+++ Speed info and S calculator/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int speed = int.Parse(Console.ReadLine());
+            if (speed <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+            else if( speed <= 50 )
+            {
+                Console.WriteLine("average");
+            }
+            else if (speed <= 100)
+            {
+                Console.WriteLine("fast");
+            }
+            else if (speed <= 1000)
+            {
+                Console.WriteLine("super fast");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Speed info and S calculator/ConsoleApp1/S Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("square, rectangle, triangle or circle");
+            string shape = Console.ReadLine();
+            double a;
+            double b;
+            double ha;
+            switch (shape)
+            {
+                case "square":
+                    Console.Write("a = ");
+                    a = double.Parse(Console.ReadLine());
+                    Console.WriteLine(a*a);
+                    break;
+                case "rectangle":
+                    Console.Write("a = ");
+                    a = double.Parse(Console.ReadLine());
+                    Console.Write("b = ");
+                    b = double.Parse(Console.ReadLine());
+                    Console.WriteLine(a * b);
+                    break;
+                case "triangle":
+                    Console.Write("a = ");
+                    a = double.Parse(Console.ReadLine());
+                    Console.Write("ha= ");
+                    ha = double.Parse(Console.ReadLine());
+                    Console.WriteLine(a * ha / 2);
+                    break;
+                case "circle":
+                    Console.Write("r = ");
+                    double r = double.Parse(Console.ReadLine());
+                    Console.WriteLine(Math.Round( r * r * 3.14 , 2));
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
