b7d8ad21309f72b5a0c27852f38aa682acef42a0
Nikita Koynov <nikicha03@gmail.com>
2018-10-24 23:52:26 +0300

    from (10) to (2)

---
+++ My projects/from (10) to (2)/from (10) to (2).cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number10 = int.Parse(Console.ReadLine());
+            int number2 = 0;
+            int io = 1;
+            while (number10 != 0)
+            {
+                number2 = number2 + ((number10 % 2) * io);
+                number10 = number10 / 2;
+                io = io * 10;
+            }
+            Console.WriteLine(number2);
+            Console.ReadLine();
+        }
+    }
+}
