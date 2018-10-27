d337978849191bc13aabea1f0012470565a5a550
Nikita Koynov <nikicha03@gmail.com>
2018-10-27 09:55:51 +0300

    26.10.2018

---
+++ 26.10.2018/Izvejda chislo/Izvejda chislo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Izvejda_chislo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            number = Math.Abs(number);
+            for (int i = 0; i < number; i++)
+            {
+                int counter = 0;
+                int n = i;
+                while ( n != 0 )
+                {
+                    counter = counter + n % 10;
+                    n = n / 10;
+                }
+                if (counter == 5 || counter == 7 || counter == 11)
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 26.10.2018/Izvejda chislo/Sud za voda/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sud_za_voda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your collector is 255ml.");
+            int counter = 0;
+            int counter2 = 0;
+            int cup;
+            int collector = 0;
+            do
+            {
+                cup = int.Parse(Console.ReadLine());
+                if (cup <= (255 - collector))
+                {
+                    collector += cup;
+                    counter++;
+                }
+                else
+                {
+                    Console.WriteLine("There is no enough space!");
+                }
+                counter2++;
+            } while (collector != 255);
+            Console.WriteLine("The collector is full!");
+            Console.WriteLine("{0}/{1}", counter, counter2);
+            Console.ReadLine();
+        }
+    }
+}
