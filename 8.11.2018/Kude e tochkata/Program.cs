ae9337879fddad3427ef9de536ede4d6899a3875
Nikita Koynov <nikicha03@gmail.com>
2018-11-08 21:25:02 +0200

    8.11.2018

---
+++ 8.11.2018/Kude e tochkata/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kude_e_tochkata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, x2, x, y1, y2, y;
+
+            do
+            {
+                Console.WriteLine("Vuvedete usporednika");
+                Console.WriteLine("Vuvedete gornata lqva tochka");
+                Console.Write("x1 = ");
+                x1 = double.Parse(Console.ReadLine());
+                Console.Write("y1 = ");
+                y1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Vuvedete dolnata dqsna tochka");
+                Console.Write("x2 = ");
+                x2 = double.Parse(Console.ReadLine());
+                Console.Write("y2 = ");
+                y2 = double.Parse(Console.ReadLine());
+            } while (x1 > x2 || y1 < y2);
+
+            Console.WriteLine("Vuvedete tochka, za koqto shte se izvurshva proverka");
+            Console.Write("x = ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            y = double.Parse(Console.ReadLine());
+
+            bool inside = x1 < x && x < x2 && y1 > y && y > y2;
+            bool outside = x1 > x || x > x2 || y1 < y || y < y2;
+
+            if(inside)
+            {
+                Console.WriteLine("Tochkata e v pravougalnika");
+            }
+            else if (outside)
+            {
+                Console.WriteLine("Tochkata e izvun pravougalnika");
+            }
+            else
+            {
+                Console.WriteLine("Tochkata e vurhu strana ot pravougulnika");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 8.11.2018/Prihodi ot bileti/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prihodi_ot_bileti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ColumnNumber = int.Parse(Console.ReadLine());
+            int SeatsNumber = int.Parse(Console.ReadLine());
+            string Type = Console.ReadLine().ToLower(); // premiere, normal, discount
+
+            switch (Type)
+            {
+                case "premiere":
+                    Console.WriteLine(Math.Round(12.00 * ColumnNumber * SeatsNumber, 2));break;
+                case "normal":
+                    Console.WriteLine(Math.Round(7.50 * ColumnNumber * SeatsNumber, 2)); break;
+                case "discount":
+                    Console.WriteLine(Math.Round(5.60 * ColumnNumber * SeatsNumber, 2)); break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
