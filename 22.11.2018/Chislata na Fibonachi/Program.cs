7590a662b1170d9bab1d0b85ae5d25829f093d9b
Nikita Koynov <nikicha03@gmail.com>
2018-11-22 19:30:29 +0200

    22.11.2018
    Stulba ot chisla i Chislata na Fibonachi

---
+++ 22.11.2018/Chislata na Fibonachi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chislata_na_Fibonachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int number1 = 0;
+            int number2 = 1;
+
+            while (number1 < n || number2 < n)
+            {
+                if (number1 < n)
+                {
+                    Console.Write($"{number1} ");
+                    number1 += number2;
+                }
+
+                if (number2 < n)
+                {
+                    Console.Write($"{number2} ");
+                    number2 += number1;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 22.11.2018/stulba ot chisla/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stulba_ot_chisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int nCoppy = n;
+            int number = 1;
+            int line = 0;
+
+            while ( n > 0 )
+            {
+                line++;
+                for (int i = 1; i <= line; i++)
+                {
+                    Console.Write($"{number} ");
+                    number++;
+                    if (number > nCoppy) break;
+                }
+                Console.WriteLine();
+                n -= line;
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
