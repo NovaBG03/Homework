4f635637dd6208878605cfb07c369c20dfdf712f
Nikita Koynov <nikicha03@gmail.com>
2018-11-24 12:01:00 +0200

    23.11.2018
    homework

---
+++ 23.11.2018/Duma v indeksi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duma_v_indeksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            char[] letters = Console.ReadLine().ToLower().ToCharArray();
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (letters[i] == alphabet[j])
+                    {
+                        Console.Write("{0} ",j + 1);
+                        break;
+                    }
+                }
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sedmicata na razlichni ezici/23.11.2018/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23._11._2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day;
+            do
+            {
+                day = int.Parse(Console.ReadLine());
+                if( day > 0 && day < 8 )
+                {
+                    day--;
+                    break;
+                }
+                Console.WriteLine("Enter correct day number!");
+            } while (true);
+            string language = Console.ReadLine(); //en gr bg fr ru
+            language = language.ToLower();
+
+            string[] en = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            string[] gr = { "Δευτέρα", "Τρίτη", "Τετάρτη", "Πέμπτη", "Παρασκευή", "Σάββατο", "Κυριακή" };
+            string[] bg = { "Понеделник", "Вторник", "Сряда", "Четвъртък", "Петък", "Събота", "Неделя" };
+            string[] fr = { "Le lundi", "Mardi", "Le mercredi", "Jeudi", "Vendredi", "Samedi", "Le dimanche" };
+            string[] ru = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+            switch (language)
+            {
+                case "en": Console.WriteLine(en[day]); break;
+                case "gr": Console.WriteLine(gr[day]); break;
+                case "bg": Console.WriteLine(bg[day]); break;
+                case "fr": Console.WriteLine(fr[day]); break;
+                case "ru": Console.WriteLine(ru[day]); break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
