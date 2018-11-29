914e5887d664c0c7d4e0d3d5f89d2339d846f160
Nikita Koynov <nikicha03@gmail.com>
2018-11-29 21:30:40 +0200

    29.11.2018
    kvadrat chisla

---
+++ 29.11.2018 kubche ot chisla/kubche ot chisla/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kubche_ot_chisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int counter = 0;
+
+                for (int j = i; j <= n; j++)
+                {
+                    Console.Write(j + " ");
+                    counter++;
+                }
+
+                for (int j = n - 1; j >= counter; j--)
+                {
+                    Console.Write(j + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
